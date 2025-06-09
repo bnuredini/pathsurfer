5885a7dadd5787bd50734a85602c3ca347eee987
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-06-09 22:23:17 +0200

    feat: add script for fish

---
+++ scripts/psurf.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+function psurf
+    # TODO: set bin path here
+    set app_path "/home/bleart/code/tmp/pathsurfer/bin/pathsurfer"
+    
+    if not test -x "$app_path"
+        echo "error: pathsurfer not found or not executable at $app_path"
+        return 1
+    end
+
+    set target_dir ("$app_path")
+
+    if test -n "$target_dir"; and test -d "$target_dir"
+        cd "$target_dir"
+    end
+end
+
