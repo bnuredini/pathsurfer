946d5f359e32e935a51617cef5b1a428f7560a1a
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-10-28 14:09:14 +0100

    fix: typo in the shell function name

--- scripts/psurf.sh
+++ scripts/psurf.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-psnav() {
+psurf() {
     # TODO: set bin path here
     local app_path="/home/bleart/code/pathsurfer/bin/pathsurfer"
     local target_dir
 
     if [ ! -x "$app_path" ]; then
-        echo "error: pathsurfer found or isn't executable at $app_path"
+        echo "error: pathsurfer not found or isn't executable at $app_path"
         return 1
     fi
-    
+
     target_dir=$("$app_path")
 
     if [ -n "$target_dir" ] && [ -d "$target_dir" ]; then
         cd "$target_dir"
     fi
 }
-
