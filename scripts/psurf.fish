20213bc348a082a3cf90bd272c89ffb7382a336f
Bleart Nuredini <nuredini.bleart@gmail.com>
2025-12-15 17:47:42 +0100

    fix: use new app path in shell scripts

--- scripts/psurf.fish
+++ scripts/psurf.fish
@@ -1,15 +1,14 @@
 #!/usr/bin/env fish
 
 function psurf
-    # TODO: set bin path here
-    set app_path "/home/bleart/code/pathsurfer/bin/pathsurfer"
+    set app_path "/usr/bin/pathsurfer"
     
     if not test -x "$app_path"
         echo "error: pathsurfer not found or isn't executable at $app_path"
         return 1
     end
 
-    set target_dir ("$app_path")
+    set target_dir ("$app_path" $argv)
 
     if test -n "$target_dir"; and test -d "$target_dir"
         cd "$target_dir"
